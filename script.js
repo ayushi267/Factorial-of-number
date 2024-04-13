0470e197c4a06d5315cceb1ac210d63d17d9b1d9
Ayushii <148005670+ayushi267@users.noreply.github.com>
2024-04-13 13:43:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let a = 7
+function factorial(number){
+   let arr = Array.from(Array(number+1).keys())
+   let c = arr.slice(1,).reduce((a, b)=>{
+      return a*b })
+   return c
+}
+console.log(factorial(a))
